7bcb728d158a66f95fbaf0d1887674766d8bea0c
Lucas <lucas.p.oliveira@opah.com.br>
2021-06-01 11:34:55 -0300

    Adicionar arquivos de projeto.

---
+++ App/Index.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace App
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Stateless
+            Response.Charset = "UTF-8";
+            Response.Write("Olá");
+        }
+    }
+}
